6fce68c5c3de1eaf9a401aa5a9387f8ffaa7f87e
Royal-13 <gunjanbaunthiyal17@gmail.com>
2022-10-05 15:14:54 +0530

    modified js

--- app.js
+++ app.js
@@ -3,40 +3,23 @@ var txtInput = document.querySelector("#txt-input");
 var outputD = document.querySelector("#txt-output");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(a){
+function getURL(a){
     return serverURL + "?" + "text=" + a
 }
 
 function errorHandler(error){
     console.log("error occured", error);
     alert("Something went wrong with the server! Try again after some time")
-
 }
 
-/*Method1
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    var txt = txtInput.value;  //taking input
-
-    fetch(getTranslationURL(txt))
-    .then(response =>  response.json())
-    .then(json => {
-        var translatedText = json.contents.translated;
-        outputD.innerText = translatedText;
-    })
-    .catch(errorHandler)
-};*/
-
-//outputD.innerText = "";
-
-//Or method2 
 function clickHandler(){
-    var txt = txtInput.value;  //taking input
+    var txt = txtInput.value;
 
-    fetch(getTranslationURL(txt))
+    fetch(getURL(txt))
         .then(response =>  response.json())
         .then(json => {
             var translatedText = json.contents.translated;
-            outputD.innerText = translatedText;   //output
+            outputD.innerText = translatedText;  
         })
         .catch(errorHandler)
 };
